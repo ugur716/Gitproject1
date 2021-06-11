b6e97bfbce335bcae723fe3621a8e9c5a12a7095
ugur716 <karadenizugur716@gmail.com>
2021-06-11 20:42:43 +0300

    Added more work

--- src/test/java/work1/hello.java
+++ src/test/java/work1/hello.java
@@ -3,5 +3,6 @@ package work1;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("created first git reporsity");
     }
 }
