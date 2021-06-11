9e3f5ed68bc96f3a91d6a59fa7dcf994dbeb79c2
ugur716 <karadenizugur716@gmail.com>
2021-06-11 21:01:46 +0300

    Updated more hello feature

--- src/test/java/work1/hello.java
+++ src/test/java/work1/hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("created first git reporsity");
+        System.out.println("github Repo is already published to github");
+        System.out.println("doing more work");
     }
 }
